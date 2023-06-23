9b9e355f2f21d7966a34cef890b318847ae510e6
MauricioAires <mauricioaires015@gmail.com>
2023-06-23 17:15:26 -0300

    feat(BookCard) create component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,6 @@
 import { Menu } from '@/components/Menu'
+import { Star } from 'phosphor-react'
+import Image from 'next/image'
 
 import * as S from './styles'
 
@@ -6,7 +8,36 @@ export default function Home() {
   return (
     <S.Container>
       <Menu />
-      <h1>Mauricio Aires</h1>
+
+      <S.BookCard>
+        <S.BookCover>
+          <Image
+            alt="capa do livro: o poder do habito"
+            src="/images/books/o-poder-do-habito.png"
+            fill
+          />
+        </S.BookCover>
+        <S.BookContent>
+          <header>
+            <span>Há 2 dias</span>
+            <div>
+              <Star weight="fill" />
+              <Star weight="fill" />
+              <Star weight="fill" />
+              <Star />
+              <Star />
+            </div>
+          </header>
+          <section>
+            <h3>Entendendo Algoritmos</h3>
+            <span>Aditya Bhargava</span>
+            <p>
+              Nec tempor nunc in egestas. Euismod nisi eleifend at et in
+              sagittis. Penatibus id vestibulum imperdiet a at imperdiet lectu
+            </p>
+          </section>
+        </S.BookContent>
+      </S.BookCard>
     </S.Container>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -5,3 +5,50 @@ export const Container = styled('div', {
   flexDirection: 'row',
   padding: '$4',
 })
+
+export const BookCard = styled('div', {
+  display: 'flex',
+  flexDirection: 'row',
+  gap: '$4',
+
+  background: '$gray-600',
+  height: 'min-content',
+
+  padding: '$5 $6',
+  borderRadius: '$md',
+})
+
+export const BookContent = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: '$4',
+
+    span: {
+      color: '$gray-400',
+    },
+  },
+
+  section: {
+    span: {
+      display: 'block',
+      color: '$gray-400',
+      marginBottom: '$space$4',
+    },
+
+    p: {
+      whiteSpace: 'break-spaces',
+      color: '$gray-300',
+    },
+  },
+})
+
+export const BookCover = styled('div', {
+  position: 'relative',
+  width: '108px',
+  height: '152px',
+  borderRadius: '$md',
+})
